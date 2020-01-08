e459d3714ec13188b0c21ca9e8c9faf2610c640b
Ghouagh-EXT <uic56999@contiwan.com>
2020-01-08 16:34:11 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles.css";
+import Table from "./Table";
+import Form from "./Form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    characters: [
+    
+    ]
+  };
+
+  handleSubmit = character => {
+    this.setState({ characters : [...this.state.characters, character] })
+  }
+  
+  removeCharacter = index => {
+    const { characters } = this.state;
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+    // const listofchar = this.state.characters.filter((character, i) => {
+    //   return i !== 2;
+    // });
+    // console.log(listofchar);
+  };
+  render() {
+    console.log(this.state.characters);
+    const { characters } = this.state;
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App;
